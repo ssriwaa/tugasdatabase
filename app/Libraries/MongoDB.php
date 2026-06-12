3e26f3040e51cda75ff3a9377888cfc9c06fa11f
ssriwa <sriwaaayn@gmail.com>
2026-06-12 18:25:09 +0700

    Update koneksi MongoDB Atlas

--- app/Libraries/MongoDB.php
+++ app/Libraries/MongoDB.php
@@ -8,7 +8,7 @@ class MongoDB
 {
     public function connect()
     {
-        $client = new Client("mongodb://127.0.0.1:27017");
+        $client = new Client("mongodb+srv://sriwaaayn_db_user:4N14j6VXF9glfx3S@cluster0.svzwjko.mongodb.net/?appName=Cluster0");
 
         return $client->ci4_upload;
     }
